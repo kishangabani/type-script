43aae3d8424e172f08e18753cd1d1faedfa59f3c
kishangabani <kishan.gabani600@gmail.com>
2018-12-11 14:11:09 -0500

    add export modules file

---
+++ point.ts
@@ -0,0 +1,9 @@
+
+// // export modules
+export  class Point{
+    constructor(private _x?:number, private _y?:number){
+    }
+    draw(){
+        console.log('X: '+this._x+' Y: '+this._y);
+    }   
+}
